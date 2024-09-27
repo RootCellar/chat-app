b34eb75fc40d4b21dffb00b15023044cf3c404c7
RootCellar <RootCellar9877@gmail.com>
2024-09-27 09:24:29 -0800

    implement initial client object and test

--- src/Client.py
+++ src/Client.py
@@ -2,20 +2,68 @@
 import SocketHandler
 import time
 
-if __name__ == "__main__":
-    sock = SocketHandler.SocketHandler()
-    sock.connect("localhost", 45000)
+class Client(object):
 
-    sock.write("Hi")
+    def __init__(self):
+        self.serverhost = None
+        self.serverport = None
+        self.socket = None
+
+    def connect(self, host, port):
+        self.socket = SocketHandler.SocketHandler()
+
+        try:
+            self.socket.connect(host, port)
+        except ConnectionRefusedError:
+            self.socket = None
+            return False
+
+        self.serverhost = host
+        self.serverport = port
+        return True
+
+    def disconnect(self):
+        self.socket.close()
+        self.socket = None
+
+        self.serverhost = None
+        self.serverport = None
+
+    def is_connected(self):
+        if self.socket is None:
+            return False
+        return self.socket.is_connected()
+
+    def read(self):
+        if self.is_connected() is False:
+            return None
+        return self.socket.read()
+
+    def write(self, message):
+        if self.is_connected() is False:
+            return
+        self.socket.write(message)
+
+
+
+if __name__ == "__main__":
+    client = Client()
+    connected = client.connect("localhost", 45000)
+
+    if connected is False:
+        print("Could not connect to server!")
+        exit(1)
+
+    client.write("Hi")
 
     while True:
 
         time.sleep(0.1)
 
-        if sock.is_connected() == False:
+        if client.is_connected() == False:
             print("Disconnected from server")
             exit(0)
 
-        message = sock.read()
+        message = client.read()
         if message is not None:
             print(message)
