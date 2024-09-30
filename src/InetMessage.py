de4c199d96c13efbc9911923b017bc4ede2e2711
RootCellar <RootCellar9877@gmail.com>
2024-09-30 13:35:39 -0800

    implement functions to create and parse internet messages

--- src/InetMessage.py
+++ src/InetMessage.py
@@ -1,4 +1,45 @@
 
+def message_len(bytes):
+    if type(bytes) is not bytes:
+        raise RuntimeError
+
+    if len(bytes) < 4:
+        return -1
+
+    len = int.from_bytes(bytes[0:4])
+    return len
+
+def message_from_bytes(data):
+    if type(data) is not bytes:
+        raise RuntimeError
+
+    if len(data) < 8:
+        return None
+
+    length = int.from_bytes(data[0:4])
+    code = int.from_bytes(data[4:8])
+
+    if length < 1 or code < 0:
+        return InetMessage(8, -1, b'')
+
+    message = data[8:(8 + length)]
+
+    if len(message) < length:
+        return None
+
+    message = InetMessage(length + 8, code, message)
+    return message
+
+def message_to_bytes(code, data):
+    if type(data) is str:
+        message_bytes = data.encode("utf-8")
+    else:
+        raise NotImplementedError
+
+    msgLen = len(message_bytes)
+    message_bytes = int.to_bytes(msgLen, length=4) + int.to_bytes(code, length=4) + message_bytes
+
+    return message_bytes
 
 class InetMessage(object):
     def __init__(self, length, code, message):
