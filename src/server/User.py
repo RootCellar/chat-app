ae7126e08d28e4b0242a5c6a8a32cad90e6ceef7
RootCellar <RootCellar9877@gmail.com>
2024-10-08 19:56:31 -0800

    default user to INITIATED state

--- src/server/User.py
+++ src/server/User.py
@@ -1,8 +1,12 @@
 
+from ..lib.MessageType import MessageType
+from ..lib.ConnectionState import ConnectionState
+
 class User(object):
     def __init__(self, sockethandler, username = "Guest"):
         self.username = username
         self.sockethandler = sockethandler
+        self.state = ConnectionState.INITIATED
 
     def is_connected(self):
         return self.sockethandler.is_connected()
